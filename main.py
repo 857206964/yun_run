936714ced55143ddd421fcd7181ea2352b41058d
LuckCode <79688117+857206964@users.noreply.github.com>
2025-10-03 19:27:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,11 @@ def get_steps_by_time():
 # 虚拟ip地址
 def fake_ip():
     # 随便找的国内IP段：223.64.0.0 - 223.117.255.255
-    return f"{223}.{random.randint(64, 117)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
+    # 添加以39开头的IP地址
+    if random.choice([True, False]):
+        return f"39.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
+    else:
+        return f"{223}.{random.randint(64, 117)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
 
 
 # 账号脱敏
